9854dab34ead8eb09b4038f05ed0a9fe6c5e9923
André Luiz <42250150+andreluiz2431@users.noreply.github.com>
2025-05-07 13:42:13 -0300

    firebase no env

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,15 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
+import { 
+    FIREBASE_API_KEY, 
+    FIREBASE_AUTH_DOMAIN, 
+    FIREBASE_PROJECT_ID, 
+    FIREBASE_STORAGE_BUCKET, 
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID
+ } from '@env';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBiCcwPrezlagHLkkcjq5phxi2ke0Hj1RQ",
-    authDomain: "runner-coach-b745a.firebaseapp.com",
-    projectId: "runner-coach-b745a",
-    storageBucket: "runner-coach-b745a.firebasestorage.app",
-    messagingSenderId: "46370273739",
-    appId: "1:46370273739:web:d0a3423da5965bdda6de4e",
-    measurementId: "G-RW69EMGCRS"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
